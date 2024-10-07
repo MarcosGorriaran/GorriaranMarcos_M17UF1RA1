100be4398a078746b5dee0f04f284ffa1f898f5a
Marcos <marcos.gorriaran.7e7@itb.cat>
2024-10-07 16:40:29 +0200

    Started implementation of collision

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,15 +5,23 @@ public class Player : MonoBehaviour
     private static Player instance;
     [SerializeField]
     float speed = 3f;
+    [SerializeField]
+    GroundDetection groundDetect;
+    float height;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        if(instance != null)
+        if (instance != null)
         {
             Destroy(this);
         }
         instance = this;
         DontDestroyOnLoad(this);
+    }
+    void Start()
+    {
+        height = GetComponent<BoxCollider2D>().size.y / 2;
+        groundDetect.transform.localPosition = new Vector3(0, -height, 0);
     }
 
     // Update is called once per frame
@@ -27,19 +35,21 @@ public class Player : MonoBehaviour
 
             transform.GetComponent<Animator>().SetTrigger("RunTrigger");
         }
-        else if(Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))
         {
             transform.position += new Vector3(-speed, 0) * Time.deltaTime;
             transform.GetComponent<SpriteRenderer>().flipX = true;
             transform.GetComponent<Animator>().SetTrigger("RunTrigger");
         }
-        
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && groundDetect.onGround)
         {
             transform.GetComponent<Rigidbody2D>().gravityScale *= -1;
             bool flipValue = transform.GetComponent<SpriteRenderer>().flipY;
             transform.GetComponent<SpriteRenderer>().flipY = flipValue ? false : true;
+            groundDetect.transform.localPosition = new Vector3(0, flipValue ? -height : height, 0);
         }
     }
+
 }
